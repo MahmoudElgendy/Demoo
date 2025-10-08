aac2725f3158405f0c3d15b8a42e9c06d2c86e8a
Mahmoud Elgendi <Mahmoud.E@miviewis.com>
2025-10-08 16:01:44 -0500

    Use EntityTypeConfiguration

--- Demo.Api/Data/ApplicationDbContext.cs
+++ Demo.Api/Data/ApplicationDbContext.cs
@@ -12,13 +12,8 @@ namespace Demo.Api.Data
         public DbSet<Person> Persons { get; set; }
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
-            modelBuilder.Entity<Person>()
-                .OwnsMany(p => p.Qualifications, q =>
-                {
-                    q.ToJson();
-                    q.Property(l=>l.Level)
-                    .HasConversion<string>();
-                });
+            modelBuilder.ApplyConfigurationsFromAssembly(GetType().Assembly);
+            
 
         }
     }
---
+++ Demo.Api/Data/Mappings/Person_Map.cs
@@ -0,0 +1,20 @@
+﻿using Demo.Api.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System.Reflection.Emit;
+
+namespace Demo.Api.Data.Mappings
+{
+    public class Person_Map : IEntityTypeConfiguration<Person>
+    {
+        public void Configure(EntityTypeBuilder<Person> builder)
+        {
+            builder.OwnsMany(p => p.Qualifications, q =>
+                  {
+                      q.ToJson();
+                      q.Property(l => l.Level)
+                      .HasConversion<string>();
+                  });
+        }
+    }
+}
