b03c99280a3c325e3169c43ea15546aa60bcba05
Mahmoud Elgendi <Mahmoud.E@miviewis.com>
2025-10-13 15:41:37 -0500

    --

---
+++ Demo.Api/Contracts/Exceptions/DatabaseObjectNotFoundException.cs
@@ -0,0 +1,14 @@
+﻿namespace Demo.Api.Contracts.Exceptions
+{
+    public class DatabaseObjectNotFoundException : Exception
+    {
+        public DatabaseObjectNotFoundException()
+        {
+        }
+
+        public DatabaseObjectNotFoundException(string objectName, string keyName, string keyValue)
+            : base("Could not find " + objectName + " with " + keyName + "=" + keyValue)
+        {
+        }
+    }
+}
